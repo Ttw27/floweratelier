804e090659ba320f14c34bd7f6b0a355e9f98df5
Ttw27 <tim@hugehoods.co.uk>
2026-06-28 13:49:47 +0100

    Add files via upload

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,30 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   return {
-    plugins: [react()],
+    plugins: [
+      react({
+        include: '**/*.{jsx,js}',
+      })
+    ],
+    esbuild: {
+      loader: 'jsx',
+      include: /src\/.*\.js$/,
+      exclude: [],
+    },
+    optimizeDeps: {
+      esbuildOptions: {
+        loader: {
+          '.js': 'jsx',
+        },
+      },
+    },
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, './src'),
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
       },
     },
     define: {
